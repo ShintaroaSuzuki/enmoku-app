db234257e68e9fc17757a008ff691806d3eaa595
ShintaroaSuzuki <shinbo.dorapion19971221@gmail.com>
2022-06-10 00:15:42 +0900

    [add] ログアウトボタンの作成

--- pages/[userId]/index.tsx
+++ pages/[userId]/index.tsx
@@ -2,6 +2,7 @@ import useRequireAuth from "@/hooks/useRequireAuth";
 import Loader from "@/components/Loader";
 import { useUniqueUserByIdQuery } from "@/src/graphql/user/getUniqueUser.generated";
 import { useRouter } from "next/router";
+import { signOut } from "next-auth/react";
 
 const UserPage = () => {
   const session = useRequireAuth();
@@ -19,8 +20,17 @@ const UserPage = () => {
   if (error) return <p>Error</p>;
 
   return (
-    <div>
-      <p>{`ようこそ、${data?.uniqueUser.name} さん`}</p>
+    <div className="min-h-screen flex flex-col items-center gap-y-10 bg-slate-50 pt-20">
+      <p className="text-lg font-bold">{`ようこそ、${data?.uniqueUser.name} さん`}</p>
+      <div className="h-40 w-40 rounded-full overflow-hidden">
+        <img src={data?.uniqueUser.avatar} alt="profile" />
+      </div>
+      <button
+        className="rounded-md w-3/4 h-12 bg-red-800 text-slate-50"
+        onClick={() => signOut()}
+      >
+        ログアウト
+      </button>
     </div>
   );
 };
